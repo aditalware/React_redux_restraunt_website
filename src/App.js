a52185857a8349ec3133a4de2a13f36ba7869ce9
Adit Alware <alwareadit@gmail.com>
2020-05-12 00:38:22 +0530

    components menu part2

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import {Navbar,NavbarBrand} from 'reactstrap';
 import './App.css';
-import Menu from './components/MenuComponents';
-
+import Menu from './components/MenuComponents'; //app component becomes the parent of the menu component
+//we can  make available the ste of children to parent as props do that they can be available to all the children
+import {DISHES} from './shared/dishes';
 class App extends Component{
+    constructor(props)
+    {
+      super(props);
+      
+      this.state={
+        dishes:DISHES
+      };
+    }  
   render(){
     return(
       <div >
@@ -13,10 +22,11 @@ class App extends Component{
         <NavbarBrand href="/">Restorante Con Fusion</NavbarBrand>
         </div>
         </Navbar>
-        <Menu/>
+        <Menu dishes={this.state.dishes} />
     </div>
     );
   }
 }
 
 export default App;
+// dishes in Menu are props
--- src/components/MenuComponents.js
+++ src/components/MenuComponents.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {Media} from 'reactstrap';
+import {Card,CardBody,CardText,CardTitle, CardImgOverlay,CardImg} from 'reactstrap';
 
 class Menu extends Component{//name of new component for react app
 
@@ -9,59 +9,46 @@ class Menu extends Component{//name of new component for react app
 
 
         this.state = { // state is the property of that component
-            dishes: [
-                {
-                  id: 0,
-                  name:'Uthappizza',
-                  image: 'assests/images/uthappizza.png',
-                  category: 'mains',
-                  label:'Hot',
-                  price:'4.99',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.' 
-                                       },
-                                       {
-                                        id: 1,
-                                        name:'Zucchipakoda',
-                                        image: 'assests/images/zucchipakoda.png',
-                                        category: 'appetizer',
-                                        label:'',
-                                        price:'1.99',
-                                        description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
-                                     {
-                                        id: 2,
-                                        name:'Vadonut',
-                                        image: 'assests/images/vadonut.png',
-                                        category: 'appetizer',
-                                        label:'New',
-                                        price:'1.99',
-                                        description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
-                                     {
-                                        id: 3,
-                                        name:'ElaiCheese Cake',
-                                        image: 'assests/images/elaicheesecake.png',
-                                        category: 'dessert',
-                                        label:'',
-                                        price:'2.99',
-                                        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
-                                     ],
-                              };
-       
-    }
+            selectedDish:null
+             }
+            }
+       //do not change state directly bu this.state.x= something
+       onDishSelect(dish)
+       {
+           this.setState({selectedDish:dish});
+       }
+
+       renderDish(dish) 
+       {
+           if(dish!=null){
+              return(
+                  <Card>
+                  <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                  <CardBody>
+                  <CardTitle>{dish.name}</CardTitle>
+                  <CardText>{dish.description}</CardText>
+                  </CardBody>
+                  </Card>
+              );
+           }
+           else{
+               return(<div></div>);
+           }
+       }
+    
 
     render(){
 
-        const menu=this.state.dishes.map((dish)=>{
+        const menu=this.props.dishes.map((dish)=>{
             return(
-                <div key={dish.id} className="col-12 mt-5">
-                      <Media tag="li">
-                        <Media left middle>
-                           <Media object src={dish.image} alt={dish.name}/>
-                        </Media>
-                        <Media body className="ml-5">
-                           <Media heading>{dish.name}</Media>
-                           <p>{dish.description}</p>
-                        </Media>
-                      </Media>
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                      <Card onClick={()=>this.onDishSelect(dish)}>
+                        <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                           <CardImgOverlay>
+                               <CardTitle>{dish.name}</CardTitle>
+                           </CardImgOverlay>
+                      </Card>
+                     
                 </div>
             );
             //for every dish in dishes i am going to iterate and return
@@ -69,10 +56,10 @@ class Menu extends Component{//name of new component for react app
         return(
      <div className="container">
          <div className="row">
-             <Media list>
               {menu}
-             </Media>
-
+         </div>
+         <div className="container">
+            {this.renderDish(this.state.selectedDish)}
          </div>
     </div>
         );
@@ -80,3 +67,34 @@ class Menu extends Component{//name of new component for react app
 }
 
 export default Menu;//always export ur components so that it can be imported
+
+//  The below expression is for rendering media 
+// render(){
+
+//     const menu=this.props.dishes.map((dish)=>{
+//         return(
+//             <div key={dish.id} className="col-12 mt-5">
+//                   <Media tag="li">
+//                     <Media left middle>
+//                        <Media object src={dish.image} alt={dish.name}/>
+//                     </Media>
+//                     <Media body className="ml-5">
+//                        <Media heading>{dish.name}</Media>
+//                        <p>{dish.description}</p>
+//                     </Media>
+//                   </Media>
+//             </div>
+//         );
+//         //for every dish in dishes i am going to iterate and return
+//     });
+//     return(
+//  <div className="container">
+//      <div className="row">
+//          <Media list>
+//           {menu}
+//          </Media>
+
+//      </div>
+// </div>
+//     );
+// }
